ebf5ff0db020377b235c179398fbf3ee67f6168b
Nicolas Borup <nicolas.borup@gmail.com>
2025-01-29 11:40:37 +0100

    One more try

--- build.sh
+++ build.sh
@@ -31,10 +31,10 @@ dnf5 -y copr disable alternateved/keyd
 
 # Add NetworkManager-l2tp
 dnf5 install -y NetworkManager-l2tp strongswan
-rpm -e --nodeps libreswan || echo ""
+#rpm -e --nodeps libreswan || echo ""
 
 # Add tilix terminal emulator
-dnf5 install -y epel-release
+#dnf5 install -y epel-release
 dnf5 install -y tilix
 
 # Add corectrl
