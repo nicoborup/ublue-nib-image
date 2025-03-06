d1af103ab6ff5f69fd66a6a44871ab1af59a8ce1
Nicolas Borup <nicolas.borup@gmail.com>
2025-03-06 11:24:17 +0100

    re-apply changes

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 # Use a COPR Example:
 #
@@ -22,3 +22,16 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+# Add keyd
+dnf5 -y copr enable alternateved/keyd
+dnf5 -y install keyd
+# Disable COPRs so they don't end up enabled on the final image:
+dnf5 -y copr disable alternateved/keyd
+
+# Add NetworkManager-l2tp
+dnf5 install -y NetworkManager-l2tp strongswan
+#rpm -e --nodeps libreswan || echo ""
+
+# Add tilix terminal emulator
+dnf5 install -y tilix
